11824a7c224933fbeea3b69b0df78394a0f166cb
rcarrata <rcarratalasanchez@gmail.com>
2023-09-30 18:48:17 +0200

    minor fix

--- infra.tf
+++ infra.tf
@@ -53,6 +53,14 @@ resource "aws_security_group" "jupyter" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
